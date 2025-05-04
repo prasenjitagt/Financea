f5015bca1eee757bb9c57ecbe4f9ee187a49c8b1
prasanjitagt <prasanjitacademic@gmail.com>
2025-05-04 21:59:30 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,7 @@
-// next.config.ts
-import type { NextConfig } from 'next'
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      issuer: /\.[jt]sx?$/,
-      use: ['@svgr/webpack'],
-    });
-    return config;
-  },
+  /* config options here */
 };
 
 export default nextConfig;
