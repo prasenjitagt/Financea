82385d6edd7a70a110fc17913a696ab6d865c832
prasanjitagt <study.prasenjit@gmail.com>
2025-06-19 08:01:56 +0530

    minor changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,14 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+    const router = useRouter();
     return (
-        <div>Welcome to Financea Home</div>
+        <div>
+            <p>Welcome to Financea Home</p>
+            <Button onClick={() => router.push("/login")}>Log In</Button>
+        </div>
     )
 }
--- app/settings/account/page.tsx
+++ app/settings/account/page.tsx
@@ -1,7 +1,30 @@
-import React from 'react'
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { signOut } from "next-auth/react";
+
+
 
 export default function Profile() {
+
+
+    async function logOut() {
+
+        await signOut();
+
+    }
+
+
+
     return (
-        <div>Profile</div>
+        <div className="flex flex-col">
+            <p>
+                Profile
+            </p>
+            <Button className="w-15" onClick={logOut}>
+                Logout
+            </Button>
+
+        </div>
     )
 }
